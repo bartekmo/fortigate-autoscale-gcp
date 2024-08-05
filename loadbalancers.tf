7d7f423dd411476b1da84885a7170d87e8a93e85
Bartek Moczulski <12998104+bartekmo@users.noreply.github.com>
2024-08-05 18:04:37 +0200

    added ILB for outbound traffic

--- loadbalancers.tf
+++ loadbalancers.tf
@@ -1,7 +1,5 @@
 
-# External pass-through network load balancer (group-based)
-# 
-resource "google_compute_region_health_check" "elb" {
+resource "google_compute_region_health_check" "http8008" {
   name               = "${var.cluster_name}-hc-${random_string.random_name_post.result}"
   region             = var.region
   timeout_sec        = 2
@@ -12,10 +10,12 @@ resource "google_compute_region_health_check" "elb" {
   }
 }
 
+# External pass-through network load balancer (group-based)
+# 
 resource "google_compute_region_backend_service" "elb" {
   name                            = "${var.cluster_name}-elb-${random_string.random_name_post.result}"
   region                          = var.region
-  health_checks                   = [google_compute_region_health_check.elb.id]
+  health_checks                   = [google_compute_region_health_check.http8008.id]
   connection_draining_timeout_sec = 10
   load_balancing_scheme           = "EXTERNAL"
   protocol                        = "UNSPECIFIED"
@@ -32,4 +32,38 @@ resource "google_compute_forwarding_rule" "elb" {
   all_ports             = true
   load_balancing_scheme = "EXTERNAL"
   backend_service       = google_compute_region_backend_service.elb.id
+}
+
+# Internal network load balancer as dext-hop for default route from protected network
+# 
+resource "google_compute_region_backend_service" "ilb" {
+  name                            = "${var.cluster_name}-ilb-${random_string.random_name_post.result}"
+  region                          = var.region
+  health_checks                   = [google_compute_region_health_check.http8008.id]
+  connection_draining_timeout_sec = 10
+  load_balancing_scheme           = "INTERNAL"
+  protocol                        = "UNSPECIFIED"
+  network                         = google_compute_subnetwork.protected_subnet.network
+
+  backend {
+    group = google_compute_region_instance_group_manager.fgts.instance_group
+  }
+}
+
+resource "google_compute_forwarding_rule" "ilb" {
+  name                  = "${var.cluster_name}-ilb-frule-${random_string.random_name_post.result}"
+  region                = var.region
+  subnetwork            = google_compute_subnetwork.protected_subnet.id
+  ip_protocol           = "TCP"
+  all_ports             = true
+  load_balancing_scheme = "INTERNAL"
+  backend_service       = google_compute_region_backend_service.ilb.id
+  allow_global_access   = true
+}
+
+resource "google_compute_route" "default" {
+  name         = "${var.cluster_name}-default-via-fgts-${random_string.random_name_post.result}"
+  network      = google_compute_subnetwork.protected_subnet.network
+  dest_range   = "0.0.0.0/0"
+  next_hop_ilb = google_compute_forwarding_rule.ilb.id
 }
